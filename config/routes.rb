fe01b4b9e90fc3c8c05b03a55b6a30185961782c
Leonardo Mazzuca <leonardodesouzamazzuca8@gmail.com>
2025-01-05 16:49:20 -0300

    fixing sign out error

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
   }
 
-  # devise_scope :user do  
-  #   get '/users/sign_out' => 'devise/sessions#destroy'     
-  # end
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
  
 
 
